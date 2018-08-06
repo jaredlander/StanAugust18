0e255e1de5030ba2c629b542456b54205f4e01d5
Jonah Gabry <jgabry@gmail.com>
2018-08-06 00:41:48 -0400

    Update DownloadData.r
    point to newer dataset

--- prep/DownloadData.r
+++ prep/DownloadData.r
@@ -1,15 +1,9 @@
 root <- rprojroot::find_rstudio_root_file()
 dataDir <- file.path(root, 'data')
 
-# building_data.rds
+# pest_data.rds
 download.file(
-	'https://query.data.world/s/75uh3mxexj3t76sm4frsqq4m2wogi2',
-	destfile=file.path(dataDir, 'building_data.rds'),
-	mode='wb')
-
-# movie_data.rds
-download.file(
-	'https://query.data.world/s/k4pxwzi6eruaozdeau3cz4mp3lly5l',
-	destfile=file.path(dataDir, 'movie_data.rds'),
+	'https://query.data.world/s/v54wbyrbdcbeehu53d6zt7b4yglqaq',
+	destfile=file.path(dataDir, 'pest_data.rds'),
 	mode='wb')
 
